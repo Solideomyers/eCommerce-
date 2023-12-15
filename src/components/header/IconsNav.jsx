82cd3e774e8ce79d9ee437c0ea0da94a36754960
Solideomyers <franciscomyers@gmail.com>
2023-12-15 22:07:21 +0100

    FIX: El componente IconsNav ha sido modificado, mas aun no ha sido terminado por completo

--- src/components/header/IconsNav.jsx
+++ src/components/header/IconsNav.jsx
@@ -19,16 +19,16 @@ export const IconsNav = () => {
   };
 
   return (
-    <div className="flex bg-white w-fit px-1.25 py-1.25 shadow-box-up rounded-2xl dark:bg-box-dark dark:shadow-box-dark-out">
-      <div className="dark:shadow-buttons-box-dark rounded-2xl w-full px-1.5 py-1.5 md:px-3 md:py-3">
+    <div className="hidden lg:flex bg-white w-fit px-1.25 py-0 shadow-box-up rounded-2xl dark:bg-box-dark dark:shadow-box-dark-out ">
+      <div className="dark:shadow-buttons-box-dark rounded-2xl w-full px-1.5 py-0 md:px-3 md:py-1">
         <a
-          className="text-gray-700 hover:text-primary dark:text-gray-400 border-2 inline-flex items-center mr-0 last-of-type:mr-0 p-2.5 border-transparent bg-light-secondary shadow-button-flat-nopressed hover:border-2 hover:shadow-button-flat-pressed focus:opacity-100 focus:outline-none active:border-2 active:shadow-button-flat-pressed font-medium rounded-full text-sm text-center dark:bg-button-curved-default-dark dark:shadow-button-curved-default-dark dark:hover:bg-button-curved-pressed-dark dark:hover:shadow-button-curved-pressed-dark dark:active:bg-button-curved-pressed-dark dark:active:shadow-button-curved-pressed-dark dark:focus:bg-button-curved-pressed-dark dark:focus:shadow-button-curved-pressed-dark dark:border-0"
+          className="text-gray-700 hover:text-primary dark:text-gray-400 border-2 inline-flex items-center mr-0 last-of-type:mr-0 py-1 border-transparent bg-light-secondary shadow-button-flat-nopressed hover:border-2 hover:shadow-button-flat-pressed focus:opacity-100 focus:outline-none active:border-2 active:shadow-button-flat-pressed font-medium rounded-full text-sm text-center dark:bg-button-curved-default-dark dark:shadow-button-curved-default-dark dark:hover:bg-button-curved-pressed-dark dark:hover:shadow-button-curved-pressed-dark dark:active:bg-button-curved-pressed-dark dark:active:shadow-button-curved-pressed-dark dark:focus:bg-button-curved-pressed-dark dark:focus:shadow-button-curved-pressed-dark dark:border-0"
           title="Linea directa"
         >
           <PiPhoneCallFill className="w-5 h-5" />
         </a>
         <a
-          className="text-gray-700 hover:text-primary dark:text-gray-400 border-2 inline-flex items-center mr-0 last-of-type:mr-0 p-2.5 border-transparent bg-light-secondary shadow-button-flat-nopressed hover:border-2 hover:shadow-button-flat-pressed focus:opacity-100 focus:outline-none active:border-2 active:shadow-button-flat-pressed font-medium rounded-full text-sm text-center dark:bg-button-curved-default-dark dark:shadow-button-curved-default-dark dark:hover:bg-button-curved-pressed-dark dark:hover:shadow-button-curved-pressed-dark dark:active:bg-button-curved-pressed-dark dark:active:shadow-button-curved-pressed-dark dark:focus:bg-button-curved-pressed-dark dark:focus:shadow-button-curved-pressed-dark dark:border-0"
+          className="text-gray-700 hover:text-primary dark:text-gray-400 border-2 inline-flex items-center mr-0 last-of-type:mr-0 py-0 border-transparent bg-light-secondary shadow-button-flat-nopressed hover:border-2 hover:shadow-button-flat-pressed focus:opacity-100 focus:outline-none active:border-2 active:shadow-button-flat-pressed font-medium rounded-full text-sm text-center dark:bg-button-curved-default-dark dark:shadow-button-curved-default-dark dark:hover:bg-button-curved-pressed-dark dark:hover:shadow-button-curved-pressed-dark dark:active:bg-button-curved-pressed-dark dark:active:shadow-button-curved-pressed-dark dark:focus:bg-button-curved-pressed-dark dark:focus:shadow-button-curved-pressed-dark dark:border-0"
           title="Go to about me page"
         >
           <svg
@@ -45,7 +45,7 @@ export const IconsNav = () => {
           </svg>
         </a>
         <a
-          className="text-gray-700 hover:text-primary dark:text-gray-400 border-2 inline-flex items-center mr-0 last-of-type:mr-0 p-2.5 border-transparent bg-light-secondary shadow-button-flat-nopressed hover:border-2 hover:shadow-button-flat-pressed focus:opacity-100 focus:outline-none active:border-2 active:shadow-button-flat-pressed font-medium rounded-full text-sm text-center dark:bg-button-curved-default-dark dark:shadow-button-curved-default-dark dark:hover:bg-button-curved-pressed-dark dark:hover:shadow-button-curved-pressed-dark dark:active:bg-button-curved-pressed-dark dark:active:shadow-button-curved-pressed-dark dark:focus:bg-button-curved-pressed-dark dark:focus:shadow-button-curved-pressed-dark dark:border-0"
+          className="text-gray-700 hover:text-primary dark:text-gray-400 border-2 inline-flex items-center mr-0 last-of-type:mr-0 py-0 border-transparent bg-light-secondary shadow-button-flat-nopressed hover:border-2 hover:shadow-button-flat-pressed focus:opacity-100 focus:outline-none active:border-2 active:shadow-button-flat-pressed font-medium rounded-full text-sm text-center dark:bg-button-curved-default-dark dark:shadow-button-curved-default-dark dark:hover:bg-button-curved-pressed-dark dark:hover:shadow-button-curved-pressed-dark dark:active:bg-button-curved-pressed-dark dark:active:shadow-button-curved-pressed-dark dark:focus:bg-button-curved-pressed-dark dark:focus:shadow-button-curved-pressed-dark dark:border-0"
           title="Cesta de compras"
         >
           <FaShoppingBag className="w-5 h-5" />
