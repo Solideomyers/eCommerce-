eb303b566af25dc21772293313e9ade4183d38be
Solideomyers <franciscomyers@gmail.com>
2023-12-16 12:55:14 +0100

    FIX: exportacion innecesaria eliminada

--- src/redux/index.js
+++ src/redux/index.js
@@ -3,4 +3,4 @@ export { persistor } from "./store/store";
 export { categoriasApi } from "./features/api/categorias/categoriasApi";
 export { productosApi } from "./features/api/productos/productosApi";
 export { useGetCategoriasQuery } from "./features/api/categorias/categoriasApi";
-export { useGetProductosQuery } from "./features/api/productos/productosApi";
+export { useGetProductoByIdQuery } from "./features/api/productos/productosApi";
