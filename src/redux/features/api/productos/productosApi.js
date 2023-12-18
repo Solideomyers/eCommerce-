239ff057be772c9081f909a6051f1283573914ab
Solideomyers <franciscomyers@gmail.com>
2023-12-18 08:48:33 +0100

    FIX: agregado el import de la variable de entorno para el productosApi

--- src/redux/features/api/productos/productosApi.js
+++ src/redux/features/api/productos/productosApi.js
@@ -1,11 +1,11 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-// const BASE_URL = import.meta.env.VITE_API_URL_BASE;
+const BASE_URL = import.meta.env.VITE_API_URL_BASE;
 
 export const productosApi = createApi({
   reducerPath: "productosApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://laweb_api.test/api/",
+    baseUrl: BASE_URL,
   }),
   endpoints: (builder) => ({
     getProductoById: builder.query({
